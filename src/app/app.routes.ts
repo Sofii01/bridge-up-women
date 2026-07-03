fd8863d1a43d656329c1b08b8ed319282536de6e
SofiaBritps <sofiibritos01@gmail.com>
2026-07-03 12:31:02 -0300

    feat: privacy policy page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { TallerDetail } from './components/taller-detail/taller-detail';
+import { PrivacyDrawer } from './components/privacy-drawer/privacy-drawer';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'privacy-policy', component: PrivacyDrawer },
   { path: 'taller/:id', component: TallerDetail },
   { path: '**', redirectTo: '' },
 ];
--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { PrivacyDrawer } from '../privacy-drawer/privacy-drawer';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [PrivacyDrawer],
+  imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
-  privacyOpen = false;
-
   scrollTo(id: string) {
     const el = document.getElementById(id);
     if (el) el.scrollIntoView({ behavior: 'smooth' });
